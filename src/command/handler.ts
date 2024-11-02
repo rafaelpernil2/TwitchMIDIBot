rafaelpernil2/TwitchMIDIBot

commit defd5624ce8fd6fff5a7f30280511a3667c922cf

fix: Optimization to avoid onBarLoopChange regeneration on each request

src/command/queue.ts
@@ -9,9 +9,7 @@ import { GenericQueue } from '../queue/generic-queue/implementation.js';
 import { triggerChordList } from '../midi/handler.js';
 
 export const favoriteIdMap = Object.fromEntries(Object.values(Command).map((key) => [key, -1])) as Record<Command, number>;
-export let onBarLoopChange: () => Promise<void> = async () => {
-    // Implement if needed
-};
+export let onBarLoopChange: () => Promise<void>;
 
 let requestPlayingNow: { type: Command.sendloop | Command.sendchord; request: string } | null;
 
@@ -20,7 +18,12 @@ export const queueMap = {
     sendloop: new GenericQueue<Array<[noteList: string[], timeSubDivision: number]>>()
 } as const;
 
-export function createAutomaticClockSyncedQueue(targetMIDIChannel: number) {
+export function createAutomaticClockSyncedQueue(targetMIDIChannel: number): void {
+    // Only set the function once
+    if (onBarLoopChange != null) {
+        return;
+    }
+
     const onCommandTurn = (type: Command.sendloop | Command.sendchord) => async () => {
         const [turn] = queueMap[type].getCurrentTurn();
 

commit ab40bc779364e6fab8bdb728c52d5436ec2c64ca

feat: !miditimeout + fixes in queue forwarding logic
- Now every request in the queue registers the username and timestamp
- The timestamp is checked against current time to check if user is exceeding rate limit
- The queue forwarding logic did not account for skips. Now it works well and fixes automatically runtime errors when removing requests
- Config API now returns the requester user for each item in the queue
- Fixed crashing bug when setting tempo before requesting a loop or chord progression (introduced in commit defd5624ce8fd6fff5a7f30280511a3667c922cf )

Also added a macro rewards example

src/command/handler.ts
@@ -1,5 +1,5 @@
 import { ALIASES_DB, COMMAND_DESCRIPTIONS, CONFIG, ERROR_MSG, EVENT, EVENT_EMITTER, GLOBAL, PERMISSIONS_DB, REWARDS_DB, TOGGLE_MIDI_VALUES } from '../configuration/constants.js';
-import { queue, getCurrentRequestPlaying, getRequestQueue, createAutomaticClockSyncedQueue, clearAllQueues } from './queue.js';
+import { enqueue, getCurrentRequestPlaying, getRequestQueue, createAutomaticClockSyncedQueue, clearAllQueues, requestTimeout } from './queue.js';
 import { isValidCommand, deAliasCommand, splitCommandArguments } from './utils.js';
 import { CommandParams } from '../twitch/command/types.js';
 import { removeDuplicates } from '../utils/generic.js';
@@ -202,13 +202,13 @@ export async function sendnote(...[message, { targetMIDIChannel, silenceMessages
  *         twitch: { chatClient, channel, user, userRoles } // Twitch chat and user data
  *         ]
  */
-export function sendchord(...[message, { targetMIDIChannel, silenceMessages }, { chatClient, channel }]: CommandParams): void {
+export function sendchord(...[message, { targetMIDIChannel, silenceMessages }, { chatClient, channel, user, userRoles }]: CommandParams): void {
     _checkMessageNotEmpty(message);
     checkMIDIConnection();
     // Lookup previously saved chord progressions
     const chordProgression = _getChordProgression(message);
 
-    queue(message, chordProgression, Command.sendchord);
+    enqueue(message, chordProgression, user, userRoles, Command.sendchord);
     autoStartClock(targetMIDIChannel);
     createAutomaticClockSyncedQueue(targetMIDIChannel);
     sayTwitchChatMessage(chatClient, channel, [, i18n.t('SENDCHORD')], { silenceMessages });
@@ -221,12 +221,12 @@ export function sendchord(...[message, { targetMIDIChannel, silenceMessages }, {
  *         twitch: { chatClient, channel, user, userRoles } // Twitch chat and user data
  *         ]
  */
-export function sendloop(...[message, { targetMIDIChannel, silenceMessages }, { chatClient, channel }]: CommandParams): void {
+export function sendloop(...[message, { targetMIDIChannel, silenceMessages }, { chatClient, channel, user, userRoles }]: CommandParams): void {
     _checkMessageNotEmpty(message);
     checkMIDIConnection();
     // Queue chord progression petition
     const chordProgression = _getChordProgression(message);
-    queue(message, chordProgression, Command.sendloop);
+    enqueue(message, chordProgression, user, userRoles, Command.sendloop);
     autoStartClock(targetMIDIChannel);
     createAutomaticClockSyncedQueue(targetMIDIChannel);
     sayTwitchChatMessage(chatClient, channel, [, i18n.t('SENDLOOP')], { silenceMessages });
@@ -470,6 +470,22 @@ export async function midiunbanuser(...[message, { silenceMessages }, { chatClie
     await PERMISSIONS_DB.commit();
 
     sayTwitchChatMessage(chatClient, channel, [userToUnban + '\t', i18n.t('MIDIUNBANUSER_OK')], { silenceMessages });
+}
+
+/**
+ * Sets the timeout between requests per users in seconds
+ * @param commandParams [message, // Command arguments
+ *         common: { targetMIDIName, targetMIDIChannel }, // Configuration parameters
+ *         twitch: { chatClient, channel, user, userRoles } // Twitch chat and user data
+ *         ]
+ */
+export function miditimeout(...[message, { silenceMessages }, { chatClient, channel }]: CommandParams): void {
+    const newTimeout = Number(splitCommandArguments(message)[0]);
+    if (isNaN(newTimeout) || newTimeout < CONFIG.MIN_TIMEOUT || newTimeout > CONFIG.MAX_TIMEOUT) {
+        throw new Error(ERROR_MSG.INVALID_TIMEOUT());
+    }
+    requestTimeout.set(newTimeout);
+    sayTwitchChatMessage(chatClient, channel, [, i18n.t('MIDITIMEOUT_1') + ' ' + String(newTimeout) + i18n.t('MIDITIMEOUT_2')], { silenceMessages });
 }
 
 /**

src/command/queue.ts
@@ -1,5 +1,5 @@
 import { CHORD_PROGRESSIONS_KEY } from '../database/jsondb/types.js';
-import { ALIASES_DB, ERROR_MSG, EVENT, EVENT_EMITTER, GLOBAL } from '../configuration/constants.js';
+import { ALIASES_DB, CONFIG, ERROR_MSG, EVENT, EVENT_EMITTER, GLOBAL } from '../configuration/constants.js';
 import { syncMode } from '../midi/clock.js';
 import { Sync } from '../midi/types.js';
 import { areRequestsOpen } from './guards.js';
@@ -7,6 +7,10 @@ import { Command } from './types.js';
 import { ResponseStatus } from '../types/generic.js';
 import { GenericQueue } from '../queue/generic-queue/implementation.js';
 import { triggerChordList } from '../midi/handler.js';
+import { SharedVariable } from '../shared-variable/implementation.js';
+import { UserRoles } from 'src/twitch/command/types.js';
+
+export const requestTimeout = new SharedVariable<number>(CONFIG.DEFAULT_REQUEST_TIMEOUT);
 
 export const favoriteIdMap = Object.fromEntries(Object.values(Command).map((key) => [key, -1])) as Record<Command, number>;
 export let onBarLoopChange: () => Promise<void>;
@@ -57,8 +61,8 @@ export function createAutomaticClockSyncedQueue(targetMIDIChannel: number): void
  * @param type
  * @returns
  */
-export function queue(tag: string, value: Array<[noteList: string[], timeSubDivision: number]>, type: Command.sendloop | Command.sendchord): number {
-    const [turn] = queueMap[type].enqueue(tag, value);
+export function enqueue(tag: string, value: Array<[noteList: string[], timeSubDivision: number]>, requesterUser: string, { isBroadcaster }: UserRoles, type: Command.sendloop | Command.sendchord): number {
+    const [turn] = queueMap[type].enqueue(tag, value, requesterUser, { isBroadcaster });
     return turn;
 }
 
@@ -75,8 +79,8 @@ export function forwardQueue(type: Command.sendloop | Command.sendchord): void {
         return;
     }
 
-    removeFromQueue(type, currentTurn);
     queueMap[type].forward();
+    removeFromQueue(type, currentTurn);
 
     // If there's no chord progression or loop next, let's clear requestPlayingNow
     if (_isCurrentLast(Command.sendchord) && _isCurrentLast(Command.sendloop)) {

src/command/types.ts
@@ -21,7 +21,8 @@ export enum Command {
     midipause = 'midipause',
     midiresume = 'midiresume',
     midibanuser = 'midibanuser',
-    midiunbanuser = 'midiunbanuser'
+    midiunbanuser = 'midiunbanuser',
+    miditimeout = 'miditimeout'
 }
 
 export type CCCommand = [controller: number, value: number, time: number];

src/configuration/api/handler.ts
@@ -84,15 +84,15 @@ function _onRequest(authProvider: RefreshingAuthProvider, targetChannel: string)
                     case 'GET': {
                         // Obtain queue details
                         const sendChordEntries = [...queueMap.sendchord.tagEntries()[0]];
-                        const sendchord: Record<number, string> = {};
-                        for (const [turn, tag] of sendChordEntries) {
-                            sendchord[turn] = tag;
+                        const sendchord: Record<number, { requesterUser: string, tag: string }> = {};
+                        for (const [turn, tag, requesterUser] of sendChordEntries) {
+                            sendchord[turn] = { requesterUser, tag };
                         }
 
                         const sendLoopEntries = [...queueMap.sendloop.tagEntries()[0]];
-                        const sendloop: Record<number, string> = {};
-                        for (const [turn, tag] of sendLoopEntries) {
-                            sendloop[turn] = tag;
+                        const sendloop: Record<number, { requesterUser: string, tag: string }> = {};
+                        for (const [turn, tag, requesterUser] of sendLoopEntries) {
+                            sendloop[turn] = { requesterUser, tag };
                         }
 
                         const total = { sendloop, sendchord };

src/configuration/constants.ts
@@ -29,6 +29,9 @@ export const CONFIG = {
     MAX_TEMPO: 400,
     DEFAULT_TEMPO: 120,
     DEFAULT_SWEEP_FREQUENCY: 60,
+    MIN_TIMEOUT: 0,
+    MAX_TIMEOUT: 86400,
+    DEFAULT_REQUEST_TIMEOUT: 10,
     MAX_QUEUE_LENGTH: 100,
     MAX_TWITCH_MESSAGE_LENGTH: 500,
     FULFILL_TIMEOUT_MS: 10000,
@@ -54,7 +57,13 @@ export const CONFIG = {
       |_| \\_/\\_/ |_|\\__\\___|_| |_|_|  |_|_____|_____/_____|
 
                                 - Rafael Pernil (@rafaelpernil2)`,
-    DEFAULT_USER_ROLES: { isBroadcaster: false, isMod: false, isSubscriber: false, isVip: false, isFounder: false }
+    DEFAULT_USER_ROLES: (user: string, targetChannel: string) => ({
+        isBroadcaster: user === targetChannel,
+        isMod: false,
+        isSubscriber: false,
+        isVip: false,
+        isFounder: false
+    })
 };
 
 export const ERROR_MSG = {
@@ -68,9 +77,11 @@ export const ERROR_MSG = {
     BAD_MIDI_NOTE: () => i18n.t('ERROR_BAD_MIDI_NOTE'),
     INVALID_VOLUME: () => i18n.t('ERROR_INVALID_VOLUME'),
     INVALID_TEMPO: () =>
-        `${i18n.t('ERROR_INVALID_TEMPO_1')} ${CONFIG.MIN_TEMPO} ${i18n.t('ERROR_INVALID_TEMPO_2')} ${CONFIG.MAX_TEMPO} ${i18n.t('ERROR_INVALID_TEMPO_3')} ${
-            CONFIG.DEFAULT_TEMPO
+        `${i18n.t('ERROR_INVALID_TEMPO_1')} ${CONFIG.MIN_TEMPO} ${i18n.t('ERROR_INVALID_TEMPO_2')} ${CONFIG.MAX_TEMPO} ${i18n.t('ERROR_INVALID_TEMPO_3')} ${CONFIG.DEFAULT_TEMPO
         }${i18n.t('ERROR_INVALID_TEMPO_4')}`,
+    INVALID_TIMEOUT: () =>
+        `${i18n.t('ERROR_INVALID_TIMEOUT_1')} ${CONFIG.MIN_TIMEOUT} ${i18n.t('ERROR_INVALID_TIMEOUT_2')} ${CONFIG.MAX_TIMEOUT} ${i18n.t('ERROR_INVALID_TIMEOUT_3')} ${CONFIG.DEFAULT_REQUEST_TIMEOUT
+        }${i18n.t('ERROR_INVALID_TIMEOUT_4')}`,
     CHORD_PROGRESSION_NOT_FOUND: () => i18n.t('ERROR_CHORD_PROGRESSION_NOT_FOUND'),
     CHORD_PROGRESSION_BAD_INSERTION: () => i18n.t('ERROR_CHORD_PROGRESSION_BAD_INSERTION'),
     INVALID_CHORD: (chord: string) => i18n.t('ERROR_INVALID_CHORD') + ' ' + chord,
@@ -84,6 +95,7 @@ export const ERROR_MSG = {
     INVALID_REWARD: () => i18n.t('ERROR_INVALID_REWARD'),
     BAD_SETUP_PROCESS: () => i18n.t('ERROR_BAD_SETUP_PROCESS'),
     DUPLICATE_REQUEST: () => i18n.t('ERROR_DUPLICATE_REQUEST'),
+    TIMEOUT_REQUEST: (timeout: number) => `${i18n.t('ERROR_TIMEOUT_REQUEST_1')} ${timeout} ${i18n.t('ERROR_TIMEOUT_REQUEST_2')}`,
     BROADCASTER_USER_NOT_FOUND: () => i18n.t('ERROR_BROADCASTER_USER_NOT_FOUND'),
     INVALID_AFFIXES: () => i18n.t('ERROR_INVALID_AFFIXES'),
     COMMAND_MESSAGE_EMPTY: () => i18n.t('ERROR_COMMAND_MESSAGE_EMPTY'),
@@ -134,7 +146,8 @@ export const COMMAND_DESCRIPTIONS: Record<(typeof Command)[keyof typeof Command]
     midipause: () => i18n.t('HELP_MIDIPAUSE'),
     midiresume: () => i18n.t('HELP_MIDIRESUME'),
     midibanuser: () => i18n.t('HELP_MIDIBANUSER'),
-    midiunbanuser: () => i18n.t('HELP_MIDIUNBANUSER')
+    midiunbanuser: () => i18n.t('HELP_MIDIUNBANUSER'),
+    miditimeout: () => i18n.t('HELP_MIDITIMEOUT')
 } as const;
 
 export const SAFE_COMMANDS: Record<(typeof Command)[keyof typeof Command], boolean> = {
@@ -160,7 +173,8 @@ export const SAFE_COMMANDS: Record<(typeof Command)[keyof typeof Command], boole
     midipause: false,
     midiresume: false,
     midibanuser: false,
-    midiunbanuser: false
+    midiunbanuser: false,
+    miditimeout: false
 } as const;
 
 export const ALIASES_DB = new JSONDatabase<AliasesType>(CONFIG.ALIASES_DB_PATH, { ignoreFileNotFound: true });

src/midi/clock.ts
@@ -81,7 +81,7 @@ function _sendTick(output: ReturnType<JZZTypes['openMidiOut']>): () => void {
         // If is bar start and it's not executing blocking section
         if (tick === 1 && !isInProgress) {
             // Notify and send the current active mode
-            onBarLoopChange();
+            onBarLoopChange?.();
         }
 
         if (isFirst) {

src/queue/generic-queue/implementation.ts
@@ -1,15 +1,19 @@
+import { isTimestampExpired } from '../../utils/generic.js';
 import { ERROR_MSG } from '../../configuration/constants.js';
 import { ResponseStatus, Result } from '../../types/generic.js';
 import { Queue, QueueNode } from '../interface.js';
+import { requestTimeout } from '../../command/queue.js';
 
 export class GenericQueue<T> implements Queue<T> {
     private _queueMap: Map<number, QueueNode<T>>;
+    private _lastQueuePositionByUserMap: Map<string, number>;
     private _currentTurn: number;
     private _lastQueuedTurn: number;
 
     constructor() {
         // Initialize variables
         this._queueMap = new Map();
+        this._lastQueuePositionByUserMap = new Map();
         this._currentTurn = 0;
         this._lastQueuedTurn = -1;
     }
@@ -17,10 +21,10 @@ export class GenericQueue<T> implements Queue<T> {
     isMyTurn(turn: number): Result<boolean> {
         return [turn === this._currentTurn, ResponseStatus.Ok];
     }
-    tagEntries(): Result<[turn: number, tag: string][]> {
-        const tagEntries: Array<[turn: number, tag: string]> = [];
-        for (const [turn, { tag }] of this._queueMap.entries()) {
-            tagEntries.push([turn, tag]);
+    tagEntries(): Result<[turn: number, tag: string, requesterUser: string][]> {
+        const tagEntries: Array<[turn: number, tag: string, requesterUser: string]> = [];
+        for (const [turn, { tag, requesterUser }] of this._queueMap.entries()) {
+            tagEntries.push([turn, tag, requesterUser]);
         }
         return [tagEntries, ResponseStatus.Ok];
     }
@@ -60,11 +64,16 @@ export class GenericQueue<T> implements Queue<T> {
         return [nextNode == null, ResponseStatus.Ok];
     }
 
-    enqueue(tag: string, value: T): Result<number> {
+    enqueue(tag: string, value: T, requesterUser: string, { isBroadcaster }: { isBroadcaster: boolean }): Result<number> {
         // Throw error on duplicate requests
         const [lastNode] = this._getLastInQueue();
         if (tag === lastNode?.tag) {
             throw new Error(ERROR_MSG.DUPLICATE_REQUEST());
+        }
+
+        // Check if it is not broadcaster, last request has expired and can be requested (timeout logic)
+        if (!isBroadcaster && !this._hasLastRequestByUserExpired(requesterUser)) {
+            throw new Error(ERROR_MSG.TIMEOUT_REQUEST(requestTimeout.get()))
         }
 
         // Get turns
@@ -75,9 +84,15 @@ export class GenericQueue<T> implements Queue<T> {
         this._queueMap.set(insertTurn, {
             tag,
             value,
+            requesterUser,
+            timestamp: new Date(),
             previousTurn,
             nextTurn
         });
+
+        if (!isBroadcaster) {
+            this._lastQueuePositionByUserMap.set(requesterUser, insertTurn)
+        }
 
         return [insertTurn, ResponseStatus.Ok];
     }
@@ -92,11 +107,13 @@ export class GenericQueue<T> implements Queue<T> {
         if (selectedNode == null) {
             return [null, ResponseStatus.Error];
         }
-        const { previousTurn, nextTurn } = selectedNode;
+        const { previousTurn, nextTurn, requesterUser } = selectedNode;
 
         // Delete node
         const deleteOk = this._queueMap.delete(turn);
         const status = deleteOk ? ResponseStatus.Ok : ResponseStatus.Error;
+
+        this._lastQueuePositionByUserMap.delete(requesterUser)
 
         // If turn is 0, no need to move index in previous node
         if (turn === 0) {
@@ -122,7 +139,7 @@ export class GenericQueue<T> implements Queue<T> {
         return [current.value, ResponseStatus.Ok];
     }
     forward(): Result<null> {
-        this._currentTurn++;
+        this._currentTurn = this.getNextTurn()[0];
         return [null, ResponseStatus.Ok];
     }
     clear(): Result<null> {
@@ -132,8 +149,11 @@ export class GenericQueue<T> implements Queue<T> {
     getNextTurn(): Result<number> {
         const currentItem = this._queueMap.get(this._currentTurn);
         if (currentItem == null) {
-            return [this._currentTurn + 1, ResponseStatus.Ok];
+            // If current turn does not exist, get first from queueMap keys.
+            // If queueMap is empty, move to lastQueuedTurn + 1 so that new requests keep working
+            return [this._queueMap.keys().next().value ?? this._lastQueuedTurn + 1, ResponseStatus.Ok];
         }
+        // Otherwise, get next turn
         return [currentItem.nextTurn, ResponseStatus.Ok];
     }
     getCurrentTurn(): Result<number> {
@@ -154,5 +174,31 @@ export class GenericQueue<T> implements Queue<T> {
         }
 
         return [queueNode, ResponseStatus.Ok];
+    }
+    _getLastRequestByUser(requesterUser: string): QueueNode<T> | null {
+        // Check last request by user timestamp
+        const lastPosition = this._lastQueuePositionByUserMap.get(requesterUser);
+        if (lastPosition == null) {
+            return null;
+        }
+
+        const lastRequest = this._queueMap.get(lastPosition);
+
+        if (lastRequest == null) {
+            return null;
+        }
+
+        return lastRequest;
+    }
+
+    _hasLastRequestByUserExpired(requesterUser: string): boolean {
+        const lastRequestByUser = this._getLastRequestByUser(requesterUser);
+
+        // If there is no last request, it has expired and another can be requested
+        if (lastRequestByUser == null) {
+            return true;
+        }
+
+        return isTimestampExpired(lastRequestByUser.timestamp, new Date(), requestTimeout.get());
     }
 }

src/queue/interface.ts
@@ -6,7 +6,7 @@ export interface Queue<T extends CustomObject<T>> {
      * @param tag Tag or alias for the item
      * @param value Value of the item
      */
-    enqueue(tag: string, value: T): Result<number>;
+    enqueue(tag: string, value: T, requesterUser: string, { isBroadcaster }: { isBroadcaster: boolean }): Result<number>;
     /**
      * Removes item from queue
      * @param turn Position in queue
@@ -67,12 +67,14 @@ export interface Queue<T extends CustomObject<T>> {
     /**
      * Returns the tag from all items in queue in entries format
      */
-    tagEntries(): Result<Array<[turn: number, tag: string]>>;
+    tagEntries(): Result<Array<[turn: number, tag: string, requesterUser: string]>>;
 }
 
 export type QueueNode<T> = {
     tag: string;
     value: T;
+    requesterUser: string;
+    timestamp: Date;
     previousTurn: number;
     nextTurn: number;
 };

src/twitch/command/handler.ts
@@ -41,7 +41,7 @@ export const onMessageHandlerClosure = (authProvider: RefreshingAuthProvider, ch
                     authProvider,
                     user,
                     targetChannel: env.TARGET_CHANNEL,
-                    userRoles: msg?.userInfo ?? CONFIG.DEFAULT_USER_ROLES
+                    userRoles: msg?.userInfo ?? CONFIG.DEFAULT_USER_ROLES(user, env.TARGET_CHANNEL)
                 };
                 // Checks if the user has enough permissions
                 checkCommandAccess(command, twitch, source, env);

src/utils/generic.ts
@@ -92,6 +92,19 @@ export function isNullish(value: unknown): boolean {
 }
 
 /**
+ * Checks if a given timestamp is past a past timestamp + timeout in seconds
+ * @param srcTimestamp Source timestamp
+ * @param dstTimestamp Destination timestamp
+ * @param timeout Timeout in seconds
+ * @returns If timestamp is expired
+ */
+export function isTimestampExpired(srcTimestamp: Date, dstTimestamp: Date, timeout: number) {
+    const maxValidDate = srcTimestamp.getTime() + timeout * 1000;
+
+    return dstTimestamp.getTime() > maxValidDate;
+}
+
+/**
  * Splits a message in fixed size chunks
  * Except for the last one, which can be smaller
  * @param message Full message